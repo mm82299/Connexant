d64aad424cf112405334911d8ded57fbcaf312de
mm82299 <44939338+mm82299@users.noreply.github.com>
2021-10-09 10:22:10 +0100

    Add files via upload

---
+++ l10n_tn/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2020-2021 Infotech Consulting Services - ICS(<http://www.ics-tunisie.com>).
+# contact: contact@ics-tunisie.com
---
+++ l10n_tn/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Tunisia - Accounting",
+    'description': """
+        This module implements the accounting chart for Tunisia area.
+        ===========================================================    
+        It allows any company or association to manage its financial accounting.
+    """,
+    'author': "Infotech Consulting Services - ICS",
+    'website': "http://www.ics-tunisie.com",
+    'category': 'Localization',
+    'version': '13.0.0.1',
+    'depends': ['account'],
+    'data': [
+        'data/account_tax.xml',
+        'data/l10n_tn_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account_tax_template_data.xml',
+        'data/l10n_tn_chart_post_data.xml',
+    ],
+}
